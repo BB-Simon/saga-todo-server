3736650e1eef73b2bc730fd9b8b393f5a09900aa
BB-Simon <simon.chowdery@gmail.com>
2021-09-10 02:53:12 +0200

    fix cors issue

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var helmet_1 = __importDefault(require("helmet"));
+var cors_1 = __importDefault(require("cors"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var todoRoutes_1 = __importDefault(require("./routes/todoRoutes"));
 var db_1 = __importDefault(require("./db"));
@@ -14,15 +15,16 @@ var app = (0, express_1.default)();
 app.use((0, helmet_1.default)());
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
-var baseAPIPath = "/api/v1";
-// todo APIs
-app.use(baseAPIPath, todoRoutes_1.default);
+app.use((0, cors_1.default)());
 app.get("/", function (req, res) {
     res.send("Hello from Typescript");
 });
+var baseAPIPath = "/api/v1";
+// todo APIs
+app.use(baseAPIPath, todoRoutes_1.default);
 // connect to MongoDB database
 (0, db_1.default)();
-var PORT = process.env.PORT || 5000;
+var PORT = 5000;
 app.listen(PORT, function () {
-    console.log("App running on " + PORT + " in " + process.env.NODE_ENV);
+    console.log("App running on " + PORT);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
+import cors from "cors";
 import dotenv from "dotenv";
 
 import todoRoutes from "./routes/todoRoutes";
@@ -9,20 +10,24 @@ import connectDB from "./db";
 dotenv.config();
 
 const app: Express = express();
+
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Typescript");
+});
 
 const baseAPIPath = "/api/v1";
 // todo APIs
 app.use(baseAPIPath, todoRoutes);
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Typescript");
-});
+
 // connect to MongoDB database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`App running on ${PORT} in ${process.env.NODE_ENV}`);
+  console.log(`App running on ${PORT}`);
 });
